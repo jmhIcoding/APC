1d046aba5ae7c70bc4dd3c4981f2afd769fa7f37
DESKTOP-DJ29T4D\jmh081701 <1024836002@qq.com>
2019-02-23 23:50:54 +0800

    update

--- APC/APC_INJECT/inject_main.cpp
+++ APC/APC_INJECT/inject_main.cpp
@@ -13,7 +13,10 @@ int main()
 	memset(&pi, 0, sizeof(pi));
 	si.cb = sizeof(si);
 	char * DLLFILE = (char*)"C:\\Users\\jmh081701\\Documents\\APC\\APC\\x64\\Debug\\APC.dll";
-	char * appFILE =(char *) "C:\\Program Files\\internet explorer\\iexplore.exe";
+	char * chrome =(char *) "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+	char *ie = (char *)"C:\\Program Files\\internet explorer\\iexplore.exe";
+	char *client = (char *)"C:\\Users\\jmh081701\\Documents\\APC\\APC\\x64\\Debug\\client.exe";
+	char * appFILE = client;
 	if (TRUE == DetourCreateProcessWithDll(NULL,appFILE, 
 		NULL, NULL, FALSE, 
 		NORMAL_PRIORITY_CLASS , NULL, NULL, &si, &pi, DLLFILE, NULL)
@@ -25,10 +28,6 @@ int main()
 	{
 		fprintf(stderr, "Inject %s error.\n", appFILE);
 		fprintf(stderr, "Error ID:%d\n", GetLastError());
-	}
-	while (true)
-	{
-		;
 	}
 	return 0;
 }
